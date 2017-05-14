36ea2f7b0c93b0f32cadde19fb029e460e37d07b
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2017-05-14 21:15:10 +0200

    even more debug

--- deployment_scripts/composer_install.sh
+++ deployment_scripts/composer_install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 php_fpm_container_id=$(docker ps | grep php71-fpm | cut -f 1 -d ' ')
+docker exec -u luqo33 ${php_fpm_container_id} ls -al
 docker exec -u luqo33 ${php_fpm_container_id} composer install
