ca93e4fba606f05f8d3e07f0a2da3a6e5df75ae3
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2017-05-14 20:48:32 +0200

    trigger whole pipeline

--- deployment_scripts/composer_install.sh
+++ deployment_scripts/composer_install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-php_fpm_container_id=$(docker ps | grep php-fpm | cut -f 1 -d ' ')
-docker exec ${php_fpm_container_id} composer install
+php_fpm_container_id=$(docker ps | grep php71-fpm | cut -f 1 -d ' ')
+docker exec -u luqo33 ${php_fpm_container_id} composer install
