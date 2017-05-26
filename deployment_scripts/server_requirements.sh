e9e095262dac1ba280d5ec335e5eb323c141a906
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2017-05-26 22:19:01 +0200

    fix deployment

--- deployment_scripts/server_requirements.sh
+++ deployment_scripts/server_requirements.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 php_container=$(docker ps | grep php71-fpm | cut -f 1 -d ' ')
-docker exec -u luqo33 ${php_container} php bin/symfony_requirements
+docker exec -u www-data ${php_container} php bin/symfony_requirements
