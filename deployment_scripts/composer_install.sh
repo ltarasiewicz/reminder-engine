113b1074d719ea3663f790e3cdec24dcc14275fc
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2017-05-26 20:57:19 +0200

    fix composer install

--- deployment_scripts/composer_install.sh
+++ deployment_scripts/composer_install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 php_container=$(docker ps | grep php71-fpm | cut -f 1 -d ' ')
-docker exec -u luqo33 ${php_container} composer install --no-dev --optimize-autoloader
+docker exec -u luqo33 ${php_container} composer install --no-dev --optimize-autoloader --no-scripts
