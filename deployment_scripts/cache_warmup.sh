b32932432508d91acd377c4238f80bbd2d3f8033
ltarasiewicz <lukasz.tarasiewicz@polcode.net>
2017-05-28 13:13:17 +0200

    d

---
+++ deployment_scripts/cache_warmup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# export MONGODB_CONNECTION_STRING=$(aws ssm get-parameters --region ap-southeast-2 --names mongodb-connection-string --query Parameters[0].Value)
+# export MAILHUB=$(aws ssm get-parameters --region ap-southeast-2 --names mailhub --query Parameters[0].Value)
+# export AUTH_USER=$(aws ssm get-parameters --region ap-southeast-2 --names auth-user --query Parameters[0].Value)
+# export AUTH_PASS=$(aws ssm get-parameters --region ap-southeast-2 --names auth-pass --query Parameters[0].Value)
+
+php_container=$(docker ps | grep php71-fpm | cut -f 1 -d ' ')
+docker exec -u $(id -u $USER) ${php_container} php bin/console cache:warmup --env=prod --no-debug
+docker exec -u $(id -u $USER) ${php_container} chmod -R 777 var/
